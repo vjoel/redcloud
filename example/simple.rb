70028d49f7df7c122107a304f0ecdd6a30b58406
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-02-20 17:14:56 -0800

    add latency estimate

--- example/simple.rb
+++ example/simple.rb
@@ -18,9 +18,11 @@ class Server < Component
   link client: Client
   continuous :capacity  # req per second handled by server
   continuous :backlog   # number of req not yet handled
+  continuous :latency   # expected seconds to process a new request
 
   flow do
     diff " backlog' = client.request_rate - capacity "
+    alg  " latency  = backlog / capacity "
   end
   
   transition do
@@ -39,9 +41,11 @@ server = w.create(Server) {|s|
 }
 
 backlog = [[w.clock, server.backlog]]
+latency = [[w.clock, server.latency * 1000]]
 
 w.evolve 20.0 do
   backlog << [[w.clock, server.backlog]]
+  latency << [[w.clock, server.latency * 1000]]
 end
 
 if ARGV.delete('-p')
@@ -50,6 +54,7 @@ if ARGV.delete('-p')
 
   gnuplot do |plot|
     plot.add backlog, 'title "backlog" with lines'
+    plot.add latency, 'title "latency (ms)" with lines'
   end
 else
   puts "use -p switch to show plot"
